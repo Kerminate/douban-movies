96e2ae61f5bd4d97eb6efed7cb81d2fbb71564cf
Kerminate <kepeilin@outlook.com>
2018-12-03 09:42:17 +0800

    feat: add ejs and pug

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 const Koa = require('koa')
+const views = require('koa-views')
+const path = require('path')
 
 const app = new Koa()
+
+// const ejs = require('ejs')
+// const pug = require('pug')
+// const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+
+// app.use(async (ctx, next) => {
+//   ctx.type = 'text/html; charset=utf-8'
+//   ctx.body = pug.render(pugTpl, {
+//     you: 'KPL',
+//     me: 'Kerminate'
+//   })
+// })
+
+app.use(views(path.resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
+
 app.use(async (ctx, next) => {
-  ctx.body = 'hello'
+  await ctx.render('index', {
+    you: 'James',
+    me: 'Kerminate'
+  })
 })
 
 app.listen(3000, () => {
